6ee67221cbf0e34ba706cd57f1c2940a9cc3f253
Gray Loki <kutsencko.sergey2014@yandex.ru>
2022-06-23 21:22:03 +0300

    Created applicationContext and tested first bean

---
+++ src/main/java/ua/grayloki8/spring/TestSpring.java
@@ -0,0 +1,11 @@
+package ua.grayloki8.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("applicationContext");
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+        System.out.println(testBean.getName());
+    }
+}
