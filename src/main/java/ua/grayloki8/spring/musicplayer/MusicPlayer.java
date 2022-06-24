64ebbb6e29a7d6aecf5eacea0cefa5a606deab1a
Gray Loki <kutsencko.sergey2014@yandex.ru>
2022-06-24 20:51:58 +0300

    DI with help setter

--- src/main/java/ua/grayloki8/spring/musicplayer/MusicPlayer.java
+++ src/main/java/ua/grayloki8/spring/musicplayer/MusicPlayer.java
@@ -2,11 +2,23 @@ package ua.grayloki8.spring.musicplayer;
 
 public class MusicPlayer {
     private Music music;
+
+    public MusicPlayer() {
+    }
+
     //IoC
     public MusicPlayer(Music music){
         this.music=music;
     }
     public void playMusic(){
         System.out.println("Playing: "+music.getSong());
+    }
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
     }
 }
