52e6299bf91a5b9b7d05734bfc43d98da2950d6d
Gray Loki <skuc090@gmail.com>
2022-06-25 21:43:37 +0300

    Config without XML, only Java class

---
+++ src/main/java/ua/grayloki8/spring/musicplayer/PopMusic.java
@@ -0,0 +1,8 @@
+package ua.grayloki8.spring.musicplayer;
+
+public class PopMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Pop";
+    }
+}
--- src/main/java/ua/grayloki8/spring/musicplayer/Test.java
+++ src/main/java/ua/grayloki8/spring/musicplayer/Test.java
@@ -1,13 +1,18 @@
 package ua.grayloki8.spring.musicplayer;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ua.grayloki8.spring.test.TestBean;
 
+import java.lang.annotation.Annotation;
+
 public class Test {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("applicationContext");
+        AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(SpringConfig.class);
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
       musicPlayer.playMusic();
+        PopMusic popMusic = context.getBean("popMusic", PopMusic.class);
+        System.out.println(popMusic.getSong());
      /*   MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
        // MusicPlayer musicPlayer=new MusicPlayer(music);
